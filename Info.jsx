6dd460c1d5811d89dd58100165cb9c9cc6749d96
GabrielNixsonJones <86356816+GabrielNixsonJones@users.noreply.github.com>
2021-06-28 11:23:54 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class="note">
+      <h1>javascript and react.js</h1>
+      <p>a basic web dev React Js Bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
